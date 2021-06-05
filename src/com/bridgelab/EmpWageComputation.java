983e511b93da706c4ec2352de08c1d5630a27cc2
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-06-05 18:18:20 +0530

    Added : Checking Employee Attendance

--- src/com/bridgelab/EmpWageComputation.java
+++ src/com/bridgelab/EmpWageComputation.java
@@ -1,7 +1,23 @@
 package com.bridgelab;
-
+import java.util.*;
+class Employee {
+	int isFullTimePresent;
+	int randomCheck;
+	public void checkAttendance() {
+		Random  random = new Random();
+		randomCheck = random.nextInt(2);
+		if(randomCheck == isFullTimePresent) {
+			System.out.println("Employee is Present");
+		}else {
+			System.out.println("Employee is Absent");
+		}
+	}
+}
 public class EmpWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Employee obj = new Employee();
+		obj.isFullTimePresent = 1;
+		obj.checkAttendance();
 	}
 }
