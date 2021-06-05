db2d2e49af1710dd7e985c5d0b9e1797b752a629
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-06-05 18:57:21 +0530

    Added : Part timr employee and salary

--- src/com/bridgelab/EmpWageComputation.java
+++ src/com/bridgelab/EmpWageComputation.java
@@ -4,29 +4,46 @@ import java.util.*;
 
 class Employee {
 	int isFullTimePresent;
+	int isPartTimePresent;
 	int randomCheck;
 	int workingHrs;
 	int wagePerHr;
 	int salary;
 
+	/**
+	 * Checking Employee is present or absent
+	 */
 	public void checkAttendance() {
 		Random random = new Random();
-		randomCheck = random.nextInt(2);
+		randomCheck = random.nextInt(3);
 		if (randomCheck == isFullTimePresent) {
-			System.out.println("Employee is Present ");
+			System.out.println("Employee is Full Time Present ");
 			workingHrs = 8;
-		} else {
+		}else if(randomCheck == isPartTimePresent)  {
+				System.out.println("Employee is Part time Present ");
+				workingHrs = 4;
+		}else {
 			System.out.println("Employee is Absent ");
 			workingHrs=0;
 		}
 	}
 
+	/**
+	 * Calculating daily salary of an employee
+	 */
 	public void calculatingDailyWage() {
-		salary = workingHrs * wagePerHr ;
+		salary = workingHrs * wagePerHr;
 	}
 }
 
 public class EmpWageComputation {
+	/**
+	 * This is main method and it is the starting point of an execution In this we
+	 * are giving welcome message and calling methods for employee attendance and
+	 * salary
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Employee obj = new Employee();
